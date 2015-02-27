79fc01a8343d2a863b041f80af48448fe9589286
Phoenix Nemo <i@phoenixlzx.com>
2015-02-28 01:08:06 +0800

    working version

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var qrcode = new QRCode("qrcode");
+
+function manipulate() {
+    var serverAddr = $("#server-address").val();
+    var serverPort = $("#server-port").val();
+    var password = $("#password").val();
+    var encryption = $("#encryption").val();
+
+    if (serverAddr && serverPort && password && encryption) {
+        var origstr = encryption + ':' + password + '@' + serverAddr + ':' + serverPort;
+        var b64str = utf8_to_b64(origstr);
+        console.log('ss://' + b64str);
+        qrcode.makeCode('ss://' + b64str);
+    } else {
+        alert("Please fill all blanks.");
+    }
+}
+
+function utf8_to_b64(str) {
+    return window.btoa(unescape(encodeURIComponent(str)));
+}
+
+$("#generate").click(function(event){
+    event.preventDefault();
+    manipulate();
+});
